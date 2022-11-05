a0e0c149b538cd524fc92d234777a73c2dff9bb0
briantoanle <68524814+briantoanle@users.noreply.github.com>
2022-11-05 02:02:59 -0400

    Progress at 2am on Saturday, still doesn't work

--- q2.py
+++ q2.py
@@ -1,7 +1,7 @@
 
 n,m=2,6
-#userinput=['1 alex','1 Alex','2 sam','1 alix','1 Alix','2 caM']
-userinput = ['3 jun','3 Jin','1 Li','2 Kitty','2 Josh','3 Bob','1 Dave','2 Jose','1 David','3 Rob','3 Anne','3 Ann','3 Kevin','2 Lara','1 ALI','3 Xin']
+userinput=['1 alex','1 Alex','2 sam','1 alix','1 Alix','2 caM']
+#userinput = ['3 jun','3 Jin','1 Li','2 Kitty','2 Josh','3 Bob','1 Dave','2 Jose','1 David','3 Rob','3 Anne','3 Ann','3 Kevin','2 Lara','1 ALI','3 Xin']
 #for i in userinput1:
     #print(i)
 '''
@@ -19,56 +19,69 @@ for i in range(int(m)):
 # li dave david ali
 # kitty josh kevin lara
 # jun bob anne ann xin
+def find_similarity(s1,s2):
+    counter=0
+    for l in range(len(s1)):
+        if s1[l] != s2[l]:
+            counter += 1
+    if counter < 2:
+        return s1
 
+#print(find_similarity('aeee','alen'))
+def main():
+    nameList =[]
+    for i in range(len(userinput)):
+        newus = userinput[i].split(' ')
+        newus[1] = newus[1].lower()
+        #print(newus[1])
+        nameList.append(newus[1])
+    print(nameList)
+    nl=[]
+    didntmakeit = []
+    # range a - z
+    # 25
+    for i in range(1,len(nameList)):
+        if len(nameList[i]) != len(nameList[i-1]):
+            nl.append(nameList[i-1])
+        else:
+            '''
+            word1 = nameList[i]
+            word2 = nameList[i-1]
+            temp0=0
+            temp1=0
+            word1 = list(word1)
+            word2 = list(word2)
+            #word2.sort()
+            #word1.sort()
+            counter=0
+            print(nameList[i])
+            print(nameList[i-1])
+            for l in range(len(word1)):
+                if word1[l] != word2[l]:
+                    counter+=1
+            print('here', word1)
+            print('here', word2)
+            for j in nameList[i]:
+                temp0+=ord(j)
+            for x in nameList[i-1]:
+                temp1+=ord(x)
+            print('counter',counter)
+            '''
+            if find_similarity(nameList[i],nameList[i-1]) != 'None':
+                didntmakeit.append(nameList[i])
+            #print('ascii',nameList[i],temp0)
+            #print('ascii2', nameList[i-1], temp1)
 
-nameList =[]
-for i in range(len(userinput)):
-    newus = userinput[i].split(' ')
-    newus[1] = newus[1].lower()
-    #print(newus[1])
-    nameList.append(newus[1])
-print(nameList)
-nl=[]
-didntmakeit = []
-# range a - z
-# 25
-for i in range(1,len(nameList)):
-    if len(nameList[i]) != len(nameList[i-1]):
-        nl.append(nameList[i-1])
-    else:
-        word1 = nameList[i]
-        word2 = nameList[i-1]
-        temp0=0
-        temp1=0
-        word1 = list(word1)
-        word2 = list(word2)
-        word2.sort()
-        word1.sort()
-        counter=0
-        print(nameList[i])
-        print(nameList[i-1])
-        for l in range(len(word1)):
-            if word1[l] != word2[l]:
-                counter+=1
-        print('here', word1)
-        print('here', word2)
-        for j in nameList[i]:
-            temp0+=ord(j)
-        for x in nameList[i-1]:
-            temp1+=ord(x)
-        print('counter',counter)
-        #print('ascii',nameList[i],temp0)
-        #print('ascii2', nameList[i-1], temp1)
+            #print(nameList[i-1],nameList[i])
+            #print(nameList[i])
 
-        #print(nameList[i-1],nameList[i])
-        #print(nameList[i])
-        didntmakeit.append(nameList[i])
-print(didntmakeit)
-print(nl)
-thirdlist=[]
-for i in range(len(nameList)):
-    for j in range(len(nameList)):
-        if nameList[i] == nameList[j]:
-            nameList[i]=''
-            continue
-#print(nameList)
+    #print(didntmakeit)
+    print(nl)
+    thirdlist=[]
+    for i in range(len(nameList)):
+        for j in range(len(nameList)):
+            if nameList[i] == nameList[j]:
+                nameList[i]=''
+                continue
+    #print(nameList)
+main()
--- q2TinLe.py
+++ q2TinLe.py
@@ -8,20 +8,81 @@ ui2 = ['3 jun','3 Jin','1 Li','2 Kitty','2 Josh','3 Bob','1 Dave','2 Jose','1 Da
 #stuEntries = ['3 jun','3 Jin',]
 # same string
 # string with 1 character difference
-co = []
-for i in range(courses):
-  co.append([])
 
-course_1=[]
-course_2=[]
-course_3=[]
+def main():
+  co = []
+  for i in range(courses):
+    co.append([])
 
-for i in ui2:
-  if "1" in i:
-    co[0].append(i.split())
-  elif "2" in i:
-    co[1].append(i.split())
+  course_1=[]
+  course_2=[]
+  course_3=[]
+
+  for i in ui2:
+    if "1" in i:
+      co[0].append(i.split())
+    elif "2" in i:
+      co[1].append(i.split())
+    else:
+      co[2].append(i.split())
+
+  #print(co[0])
+
+  listreturn = []
+  # for loop to go through array of all classes
+  for i in range(1,2):
+    # in each class
+
+
+    for j in range(len(co[i])):
+      list1 = co[i][j]
+      print(list1)
+      findNew(list1)
+      """
+        temp1 = co[i][j][1]
+        temp2 = co[i][j-1][1]
+        s1 = find_similarity(temp1, temp2)
+        if s1 != 'None':
+          listreturn.append(s1)
+      """
+      '''
+        if len(temp1)==len(temp2):
+          s1 = find_similarity(temp1,temp2)
+          if s1 != 'None':
+            listreturn.append(s1)
+        else:
+          listreturn.append(temp1)
+      '''
+        #print(temp2)
+  #print(listreturn)
+
+def compareString(s1,s2):
+  if s1.lower() == s2.lower():
+    return True
   else:
-    co[2].append(i.split())
-for i in range(len(co)):
-    print(co)
+    return False
+def findNew(mylist):
+  for i in range(len(mylist)):
+    for j in range(i + 1, len(mylist)):
+      print(mylist[i],mylist[j])
+      # if compare return true, it means strings are the same
+      #if compareString(mylist[i], mylist[j]):
+
+def find_similarity(s1, s2):
+  if len(s1)==len(s2):
+    print(s1,s2)
+    counter = 0
+    for l in range(len(s1)):
+      if s1[l] != s2[l]:
+        counter += 1
+    if counter < 2:
+      print(counter)
+      return s2
+  else:
+    print('Appending',s1)
+    return s1
+# separate user input into classes
+# go through names in each class
+#
+
+main()
